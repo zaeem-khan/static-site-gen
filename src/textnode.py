9f70c5290260126f306fa76a48b132853d925c0e
zaeem-khan <hafizzaeemkhan@gmail.com>
2025-05-13 14:27:53 -0500

    TextNode unit tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = 'normal'
-    BOLD_TEXT = 'bold'
-    ITALIC_TEXT = 'italic'
-    CODE_TEXT = 'code'
+    NORMAL = 'normal'
+    BOLD = 'bold'
+    ITALIC = 'italic'
+    CODE = 'code'
     LINK = 'link'
-    IMAGE_LINK = 'image link'
+    IMAGE = 'image'
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
